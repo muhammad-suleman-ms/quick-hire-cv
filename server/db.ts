b735db2b9c77a0676c3f36e5c925b2b3973a99c2
Muhammad Suleman <165248828+muhammad-suleman-ms@users.noreply.github.com>
2025-05-01 07:11:36 +0500

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -4,6 +4,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from "@shared/schema";
 
+
 if (!process.env.DATABASE_URL) {
   console.error("DATABASE_URL is not set!");
   throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
